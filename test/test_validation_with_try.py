24cfe017562a660a64519c0942e40a1ddefedc19
Btrenary <btrenary25@gmail.com>
2020-09-22 20:30:14 -0500

    test failing

--- test/test_validation_with_try.py
+++ test/test_validation_with_try.py
@@ -2,8 +2,9 @@ import unittest
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def test_average_exception(self):
+        with self.assertRaises(ValueError):
+            average(-90, 89, 78)
 
 
 if __name__ == '__main__':
