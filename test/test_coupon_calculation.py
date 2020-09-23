18747fd22c9a8ee3a80cfd41ad71c1ae2bf56693
Btrenary <btrenary25@gmail.com>
2020-09-22 19:47:44 -0500

    test price under ten, and test price between ten and thirty pass

--- test/test_coupon_calculation.py
+++ test/test_coupon_calculation.py
@@ -11,6 +11,16 @@ class MyTestCase(unittest.TestCase):
         self.assertAlmostEqual(2.57, coupon.calculated_price(6.01, 10, 20), 2)
 
 
+    def test_price_under_between_ten_thirty(self):
+        self.assertAlmostEqual(10.72, coupon.calculated_price(10, 5, 10), 2)
+        self.assertAlmostEqual(11.35, coupon.calculated_price(10.99, 5, 15), 2)
+        self.assertAlmostEqual(14.42, coupon.calculated_price(14.99, 5, 20), 2)
+        self.assertAlmostEqual(22.26, coupon.calculated_price(25, 10, 10), 2)
+        self.assertAlmostEqual(14.01, coupon.calculated_price(18.95, 10, 15), 2)
+        self.assertAlmostEqual(18.55, coupon.calculated_price(22.50, 10, 20), 2)
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
